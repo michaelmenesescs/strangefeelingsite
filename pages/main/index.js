da6389c0695b13af5779c32c504a35a797635257
Michael <menesesmichael91@gmail.com>
2022-01-20 00:38:21 -0500

    Updated

---
+++ pages/main/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const index = () => {
+  return <div>
+      Main Page
+  </div>;
+};
+
+export default index;
---
+++ pages/main/music/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const index = () => {
+  return <div>
+      Music Page
+  </div>;
+};
+
+export default index;
